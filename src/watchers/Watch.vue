70fba2df800671e4a57e0ca317c03a4f7ff638fb
okyungjin <okyungjin@gmail.com>
2022-04-06 17:22:26 +0900

    Add Watcher.vue for watch and watchEffect

--- src/watchers/Watch.vue
+++ src/watchers/Watch.vue
@@ -1,18 +1,18 @@
 <template>
   <h2>Options API</h2>
   <div>
-    <p>{{ opt_count }}</p>
-    <button @click="opt_count++">Increase count</button>
+    <p>{{ optCount }}</p>
+    <button @click="optCount++">Increase count</button>
   </div>
 
   <h2>Composition API</h2>
   <div>
-    <p>{{ comp_count1 }}</p>
-    <button @click="comp_count1++">Increase count1</button>
+    <p>{{ compCount1 }}</p>
+    <button @click="compCount1++">Increase count1</button>
   </div>
   <div>
-    <p>{{ comp_count2 }}</p>
-    <button @click="comp_count2++">Increase count2</button>
+    <p>{{ compCount2 }}</p>
+    <button @click="compCount2++">Increase count2</button>
   </div>
 </template>
 
@@ -23,46 +23,46 @@ export default {
   // Options API
   data() {
     return {
-      opt_count: 0,
-		}
-	},
+      optCount: 0,
+    };
+  },
   watch: {
-    opt_count(cur, prev) {
-			console.log(`Options API watch : ${prev} ==> ${cur}`);
+    optCount(cur, prev) {
+      console.log(`Options API watch : ${prev} ==> ${cur}`);
     },
   },
   // Composition API
   setup() {
-    const comp_count1 = ref(0);
-    const comp_count2 = ref(0);
+    const compCount1 = ref(0);
+    const compCount2 = ref(0);
     const state = ref('실행 중');
 
     watch(
-      comp_count1,
+      compCount1,
       (cur, prev) => console.log(`Composition API watch : ${prev} => ${cur}`),
-      {immediate: true},
+      { immediate: true },
     );
 
-    watch([comp_count1, comp_count2], (cur, prev) => {
+    watch([compCount1, compCount2], (cur, prev) => {
       console.log(`Composition API multiple watch: ${prev} => ${cur}`);
     });
 
     const stop = watchEffect(
-      () => console.log(`Composition API watchEffect Called ${comp_count2.value}`),
-      {flush: 'post'},
+      () => console.log(`Composition API watchEffect Called ${compCount2.value}`),
+      { flush: 'post' },
     );
 
     const onStop = () => {
       state.value = '중지';
       stop();
-    }
+    };
 
     return {
-      comp_count1,
-      comp_count2,
+      compCount1,
+      compCount2,
       state,
       onStop,
-    }
-  }
-}
+    };
+  },
+};
 </script>
