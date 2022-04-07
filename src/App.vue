4022b3152b35c43602e515d7a7f9b6dc22875201
okyungjin <okyungjin@gmail.com>
2022-04-07 17:10:52 +0900

    Add CustomButton component using slot

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <h1>Vue3 Code Snippets</h1>
   <p>Uncomment the component you want to display.</p>
-<!--  <Watch></Watch>-->
-<!--  <Inputs></Inputs>-->
+ <!-- <Watch></Watch> -->
+ <!-- <Inputs></Inputs> -->
+<!-- <CustomButtonTest></CustomButtonTest> -->
 </template>
 
 <script setup>
 // import Watch from './watchers/Watch.vue';
-// import Inputs from './ui/input/Inputs.vue';
+// import Input from './ui/input/Inputs.vue';
+// import CustomButtonTest from './ui/button/CustomButtonTest.vue';
 </script>
---
+++ src/ui/button/CustomButtonTest.vue
@@ -0,0 +1,17 @@
+<template>
+  <CustomButton sm>Custom Button</CustomButton>
+  <CustomButton>Custom Button</CustomButton>
+  <CustomButton lg background-white>Custom Button</CustomButton>
+  <CustomButton sm pill text-red background-black>Custom Button</CustomButton>
+  <CustomButton md pill text-green background-blue>Custom Button</CustomButton>
+  <CustomButton lg pill text-white background-green>Custom Button</CustomButton>
+</template>
+
+<script>
+import CustomButton from './CustomButton.vue';
+
+export default {
+  name: 'CustomButtonTest',
+  components: { CustomButton },
+};
+</script>
