e777d270b741aaac571c862da85b5034f400c64c
okyungjin <okyungjin@gmail.com>
2022-05-03 09:49:49 +0900

    [UI] CustomSwitch 구현

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,15 @@
   <p>Uncomment the component you want to display.</p>
  <!-- <Watch></Watch> -->
  <!-- <Inputs></Inputs> -->
-<!-- <CustomButtonTest></CustomButtonTest> -->
+ <CustomButtonTest></CustomButtonTest>
 </template>
 
-<script setup>
-// import Watch from './watchers/Watch.vue';
-// import Input from './ui/input/Inputs.vue';
-// import CustomButtonTest from './ui/button/CustomButtonTest.vue';
+<script>
+import CustomButtonTest from './ui/button/CustomButtonTest';
+
+export default {
+  components: {
+    CustomButtonTest,
+  },
+};
 </script>
